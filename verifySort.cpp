1ef439060f4c105f13d7a63822ff95df19e2f05b
JorgeGuti2 <jorgeegutierrez56615@gmail.com>
2024-08-28 12:43:52 -0700

    Update verifySort.cpp

--- verifySort.cpp
+++ verifySort.cpp
@@ -1,12 +1,3 @@
-// Jorge Gutierrez
-// CECS 325-02
-// Prog 5 - Sorting Contest (threading with thread)
-// October 25, 2022
-//
-// I certify that this program is my own original work. I did not copy
-// any part of this program from any other source.
-// I further certify that I typed each and every line of code in this program.
-
 #include <iostream>
 #include <fstream>
 #include <vector>
